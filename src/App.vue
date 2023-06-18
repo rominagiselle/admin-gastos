96ea3e760123bfe37e27e18b9fe703f2b2d18c9a
Romina <rominahelou@gmail.com>
2023-06-18 12:35:20 +0200

    Definir presupuesto

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<script setup>
+import Presupuesto from './components/Presupuesto.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>
+        Planificador de gastos
+      </h1>
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto />
+
+      </div>
+    </header>
+
+  </div>
+</template>
+
+<style>
+:root {
+  --azul: #3b82f6;
+  --blanco: #FFF;
+  --gris-claro: #F5F5F5;
+  --gris: #94a3b8;
+  --gris-oscuro: #64748b;
+  --negro: #000;
+}
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+.fijar {
+  overflow: hidden;
+  height: 100vh;
+}
+
+header {
+  background-color: var(--azul);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.contenedor-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+
+.crear-gasto {
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+}
+
+.crear-gasto img {
+  width: 5rem;
+  cursor: pointer;
+}
+
+.listado-gastos {
+  margin-top: 10rem;
+}
+
+.listado-gastos h2 {
+  font-weight: 900;
+  color: var(--gris-oscuro);
+}
+</style>
---
+++ src/components/Alerta.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="alerta"> <slot></slot></div>
+</template>
+
+<style>
+
+.alerta{
+    padding: 1rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    font-size: 1.6rem;
+    text-align: center;
+    background-color: var(--blanco);
+    border-left: .5rem solid #c42323;
+    color: #c42323;
+    margin-bottom: 2rem;
+}
+</style>
